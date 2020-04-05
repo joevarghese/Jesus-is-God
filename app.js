8ec152f54c224a6c7e509cdfc9a0c23a830a8cc6
joevarghese <joevarghese21@gmail.com>
2020-04-04 22:03:47 -0500

    99 Lines of Code - Part 1

--- app.js
+++ app.js
@@ -1,94 +1,47 @@
-var player1 = "X";
-var player2 = "O";
-let currentClass = player1;
-var moves;
-var j = 0;
-const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-]
-
-let cells = document.querySelectorAll(".row>div");
+let friends = ["STANLEY","JENNY","JUSTIN","NEAL","CHRIS"];
+//2 for loops if else statement 
 
 
 
-for (var i = 0; i < cells.length; i++) {
-  cells[i].addEventListener('click', cellClicked, { once: true })
-
-}
-
-function cellClicked() {
-  if (currentClass == player1) {
-    event.target.textContent = currentClass;
-    currentClass = player2;
-    testr();
-
-    if (checkWin(player1)) {
-      document.getElementById("x").innerHTML = "X Wins!";
+    function bars(){
+    let letters;
+    let lettersLeft;
 
 
-      for (var i = 0; i < cells.length; i++) {
-        cells[i].removeEventListener('click', cellClicked, { once: true })
-      }
+for(i=0, c=0;i==0;++c){
+    if(c==friends.length){
+        break;
+        }
+        else
+        {
+            console.log(friends[c] + ":");
+            }
 
-      for (var i = 0; i < cells.length; i++) {
-        cells[i].addEventListener('click', reset)
+for(i = 99;  i>=1 ; --i){
+    
 
-      }
+        if(i==1){
 
-      function reset() {
-        cells[i].textContent = window.location.reload(true);
+        letters = "line of code";
+        lettersLeft = "no more lines of code in the file " 
+        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[c] + " stikes one out, clears it all out, " + lettersLeft);
+     
+    }else{
+        
+      
+        letters = " lines of code";
+        lettersLeft = i - 1 + " lines of code in the file";
+        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[c] + " stikes one out, clears it all out, " + lettersLeft);
 
-      }
-    }
-
-  } else {
-    currentClass == player2;
-    event.target.textContent = currentClass;
-    currentClass = player1;
-    testr();
+         }
+             }
 
 
-    if (checkWin(player2)) {
-
-      document.getElementById("o").innerHTML = "O Wins!"
-
-      for (var i = 0; i < cells.length; i++) {
-        cells[i].removeEventListener('click', cellClicked, { once: true })
-      }
-      for (var i = 0; i < cells.length; i++) {
-        cells[i].addEventListener('click', reset)
-
-      }
-      function reset() {
-        cells[i].textContent = window.location.reload(true);
-      }
+        }        
+    
 
     }
-  }
+ 
 
-}
-
-function testr() {
-  ++j
-  if (j == 9 && checkWin(player1)) {
-
-    document.getElementById("x").innerHTML = "X Wins!";
-
-
-  } else if (j == 9) {
-    document.getElementById("draw").innerHTML = "Draw!";
-  }
-}
-
-function checkWin(player) {
-  return winCombos.some(v => v.every(k => cells[k].textContent == player));
-}
-
-
+ 
+ bars();
